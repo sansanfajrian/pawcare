f9f4d13f3d25cc4eb64a34b8760b9fe2804457bf
Sansan Fajrian <sansan.fajrian@gmail.com>
2023-01-21 17:04:42 +0700

    new seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,14 +15,11 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'role_id' => '1',
             'name' => 'Pawcare.Admin',
-            'username' => 'admin',
             'email' => 'admin@mail.com',
             'password' => bcrypt('rootadmin'),
             'remember_token' => str_random(10),
             'created_at' => '2022-05-10',
-            'updated_at' => '2022-05-10',
-
-
+            'updated_at' => '2022-05-10'
         ]);
     }
 }
