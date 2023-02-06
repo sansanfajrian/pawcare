def3ed9bf62ed3923c1b3abca7c132ce46c253b9
Sansan Fajrian <sansan.fajrian@gmail.com>
2023-02-06 21:40:31 +0700

    FIX : Doctor consultation bug

--- app/Http/Controllers/Author/ConsultationController.php
+++ app/Http/Controllers/Author/ConsultationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Author;
 
 use App\Consultation;
+use App\UserDoctorDetail;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -14,26 +15,13 @@ class ConsultationController extends Controller
 
         $user = Auth::user();
 
-        $doctor_id = $user->userDoctorDetails->id;
+        $doctor = UserDoctorDetail::where('user_id', '=', $user->id)->first();
 
         #list of consultations
         $consultations = Consultation::with(['userDoctorDetail.user', 'user'])
-            ->where('user_doctor_detail_id', $doctor_id)
+            ->where('user_doctor_detail_id', $doctor->id)
             ->where('status', '!=', "Done")
             ->get()
-            ->map(function ($item) {
-                $item = $item->toArray();
-                return [
-                    'id' => $item['id'],
-                    'patient_name' => $item['user']['name'],
-                    'gender' => $item['user']['gender'],
-                    'doctor_name' => $item['user_doctor_detail']['user']['name'],
-                    'status' => $item['status'],
-                ];
-            });
-
-        #list of consultations
-        $consultations = Consultation::with(['userDoctorDetail.user', 'user'])->get()
             ->map(function ($item) {
                 $item = $item->toArray();
                 return [
