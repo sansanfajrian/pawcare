d68b54bea73c7cdafa5d53fd9f6f6be1cd4c5428
Sansan Fajrian <sansan.fajrian@gmail.com>
2023-02-07 23:31:43 +0700

    FEAT : pay at consult detail

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -472,6 +472,7 @@ class ApiController extends Controller
                     'discounted_price' => ($consultation->userDoctorDetail->price - (($consultation->userDoctorDetail->price * $consultation->userDoctorDetail->discount)/100)),
                     'description' => $consultation->userDoctorDetail->description,
                     'max_payment_time' => Carbon::parse($consultation->created_at)->addHours(24)->format('Y-m-d h:i:s'),
+                    'pay_at' => Carbon::parse(Payment::where('consultation_id', '=', $consultation->id)->firstOrFail()->created_at)->format('Y-m-d h:i:s'),
                     'approved_at' => Carbon::parse($consultation->approved_at)->format('Y-m-d h:i:s'),
                     'rejected_at' => Carbon::parse($consultation->rejected_at)->format('Y-m-d h:i:s'),
                 ];
