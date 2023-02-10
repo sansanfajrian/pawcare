1c686deac114dfc6df9fea1f51663c85425de8d0
Sansan Fajrian <sansan.fajrian@gmail.com>
2023-02-10 16:09:23 +0700

    FIX : status selesai mengulas

--- database/migrations/2023_01_26_073253_create_consultations_table.php
+++ database/migrations/2023_01_26_073253_create_consultations_table.php
@@ -21,7 +21,7 @@ class CreateConsultationsTable extends Migration
             $table->integer('user_doctor_detail_id')->unsigned();
             $table->foreign('user_doctor_detail_id')
                 ->references('id')->on('user_doctor_details');
-            $table->enum('status', ['Menunggu Pembayaran', 'Menunggu Konfirmasi Pembayaran', 'Dibatalkan','Pembayaran Ditolak', 'Sesi Konsultasi', 'Selesai'])->default('Menunggu Pembayaran');
+            $table->enum('status', ['Menunggu Pembayaran', 'Menunggu Konfirmasi Pembayaran', 'Dibatalkan','Pembayaran Ditolak', 'Sesi Konsultasi', 'Selesai', 'Selesai Mengulas'])->default('Menunggu Pembayaran');
             $table->date('approved_at')->nullable();
             $table->date('rejected_at')->nullable();
             $table->timestamps();
